981f1cc37e872e27c399f3c9a9c55792146c62c8
isaaknico <kaasi_96@hotmail.com>
2022-12-20 23:04:35 -0600

    feat: Add fusion of navbar with desktop menu

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 console.log('Hello, world');
+
+const navEmail = document.querySelector('.account__email');
+const desktopMenu = document.querySelector('.menu-container');
+
+navEmail.addEventListener('click', toggleDesktopMenu);
+
+function toggleDesktopMenu () {
+    desktopMenu.classList.toggle('inactive');
+}
