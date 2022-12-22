897a2e819f2522a0c7b878224db05134752c03d8
isaaknico <kaasi_96@hotmail.com>
2022-12-21 23:53:59 -0600

    feat: Add fusion of navbar with mobile menu

--- main.js
+++ main.js
@@ -2,9 +2,18 @@ console.log('Hello, world');
 
 const navEmail = document.querySelector('.account__email');
 const desktopMenu = document.querySelector('.menu-container');
+const menuHamburgerIcon = document.querySelector('.header__menu-icon');
+const mobileMenuIconClose = document.querySelector('.mobile-menu__icon-close');
+const mobileMenu = document.querySelector('.mobile-menu-container');
 
 navEmail.addEventListener('click', toggleDesktopMenu);
+menuHamburgerIcon.addEventListener('click', toggleMobileMenu);
+mobileMenuIconClose.addEventListener('click', toggleMobileMenu);
 
 function toggleDesktopMenu () {
     desktopMenu.classList.toggle('inactive');
+}
+
+function toggleMobileMenu () {
+    mobileMenu.classList.toggle('inactive');
 }
